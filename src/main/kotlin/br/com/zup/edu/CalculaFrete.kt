17600382991f7e10ec6e121add817c594cb12485
raphaelarm0103 <75501869+raphaelarm0103@users.noreply.github.com>
2021-04-19 08:08:48 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/zup/edu/CalculaFrete.kt
@@ -0,0 +1,29 @@
+package br.com.zup.edu
+
+import io.grpc.ServerBuilder
+import io.grpc.stub.StreamObserver
+
+fun main() {
+
+    val server = ServerBuilder
+        .forPort(50051)
+        .addService(CalculaFreteGrpcServer())
+        .build()
+
+    server.start()
+
+    server.awaitTermination()
+}
+
+class CalculaFreteGrpcServer: FretesServiceGrpc.FretesServiceImplBase(){
+    override fun calculaFrete(request: CalculaFreteRequest?, responseObserver: StreamObserver<CalculaFreteResponse>?) {
+
+        val response = CalculaFreteResponse.newBuilder()
+            .setCep(request?.cep)
+            .setValor(130.00)
+            .build()
+
+        responseObserver?.onNext(response)
+        responseObserver?.onCompleted()
+    }
+}
